764db667d65b1e39a289edc9c157c81f58b04b49
火星大王 <lijunyandeyouxiang@163.com>
2019-12-20 22:33:55 +0800

    甚至可以aes了(虽然跑不过PHP

---
+++ c/testfiles/sha1.php
@@ -0,0 +1,13 @@
+<?php
+	function msectime(){list($msec, $sec) = explode(' ', microtime());return (float)sprintf('%.0f',(floatval($msec)+floatval($sec))*1000);}		
+	if(file_exists($argv[1]))
+	{
+		$start=msectime();		
+		$fp=fopen($argv[1],"r");
+		$str=fread($fp,filesize($argv[1]));
+		fclose($fp);
+		$fp=fopen("testfiles/sha1_encode.ans","w");
+		fwrite($fp,sha1($str));
+		fclose($fp);
+		echo 'PHP SHA1 used time:'.((msectime()-$start)/1000)."s\n";		
+	}
