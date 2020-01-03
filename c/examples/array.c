fc1674d263cdad000b6bdc4c1124fe02c48baa88
火星大王 <lijunyandeyouxiang@163.com>
2020-01-03 22:13:53 +0800

    实例规范

--- c/examples/array.c
+++ c/examples/array.c
@@ -4,4 +4,7 @@ int main()
 {
 	printf("--------------------------------" __FILE__ "--------------------------------\n");
 	jry_bl_start();
+	
+	
+	printf("\nMEMEORY:%lld\n",jry_bl_malloced_size); 	
 }
