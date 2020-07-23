8bdf83b388218c1848b890549bf919e5ce988a76
火星大王 <lijunyandeyouxiang@163.com>
2020-07-23 14:38:36 +0800

    重写test

--- examples/test2.c
+++ examples/test2.c
@@ -1,18 +1,11 @@
-#include <signal.h>
-#include <stdio.h>
-int on=0;
-void jbl_signal_callback(int sign)
+#include "main.h"
+
+int main()
 {
-	on=1;
-	for(int i=0;i<1000;++i)
-		printf("2\n");
-	exit(0);
-}
-void main()
-{
-	signal(SIGINT,jbl_signal_callback);	
-	signal(SIGTERM,jbl_signal_callback);	
-	while(1)
-		if(!on)
-			printf("1\n");
+	printf("--------------------------------" __FILE__ "--------------------------------\n");
+	jbl_start();
+	
+	
+	pchars("--------------------------------" __FILE__ "--------------------------------\n");
+	jbl_stop(); 	
 }
