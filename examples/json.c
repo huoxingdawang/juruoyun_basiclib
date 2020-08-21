af9639589187aec5ba2d410084245cb17168720c
火星大王 <lijunyandeyouxiang@163.com>
2020-08-21 23:18:59 +0800

    删除了多余的函数

--- examples/json.c
+++ examples/json.c
@@ -3,8 +3,9 @@ int main(int argc,char** argv)
 {
 	jbl_start();
 	pchars("--------------------------------" __FILE__ "--------------------------------\n");
-	FILE *fp;
-	jbl_string *s1=jbl_string_add_file(NULL,fp=fopen(((argc>1)?argv[1]:"testfiles/test.json"),"rb"));fclose(fp);
+	jbl_file * f1=jbl_file_open_chars(NULL,UC((argc>1)?argv[1]:"testfiles/test.json"),JBL_FILE_READ);
+	jbl_string *s1=jbl_file_read(f1,NULL,0,-1);
+	jbl_file_free(f1);
 //	jbl_string_view(s1);
 	
 	jbl_string_size_type i=0;
@@ -12,7 +13,7 @@ int main(int argc,char** argv)
 	jbl_var *v1=jbl_var_json_decode(NULL,s1,&i);
 
 	jbl_string *s2=jbl_var_json_encode(v1,NULL,1,0);
-	jbl_string_print(s2,fp=fopen("tmp/json.out","wb"));fclose(fp);
+	jbl_file_free(jbl_file_write(jbl_file_open_chars(NULL,UC"tmp/json.out",JBL_FILE_WRITE),s2));
 	s2=jbl_string_free(s2);
 	
 
